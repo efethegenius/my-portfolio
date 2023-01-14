0e488f59a713babed9d54ab487763f2f8103455e
efethegenius <samuelefe.37@gmail.com>
2023-01-14 11:52:35 +0100

    added node and aws skills

--- src/Skillsets.js
+++ src/Skillsets.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { FaCss3, FaGit, FaHtml5, FaNpm, FaReact } from "react-icons/fa";
+import {
+  FaCss3,
+  FaGit,
+  FaHtml5,
+  FaNpm,
+  FaReact,
+  FaNodeJs,
+  FaAws,
+} from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import Fade from "react-reveal/Fade";
 
@@ -9,12 +17,14 @@ export const Skillsets = () => {
       <h1>My Skillsets</h1>
       <Fade left cascade>
         <div className="skillsets">
+          <FaAws className="skill" />
           <FaReact className="skill" />
           <IoLogoJavascript className="skill" />
           <FaHtml5 className="skill" />
           <FaCss3 className="skill" />
           <FaGit className="skill" />
           <FaNpm className="skill" />
+          <FaNodeJs className="skill" />
         </div>
       </Fade>
     </div>
